67d5aabb2e47f1fb004f7e58f2a3e9d1203c0b87
thinkpadP1 <zhindonm@gmail.com>
2020-02-04 17:38:34 +0000

    fixed wrong path

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
 docker build -t zhindonm/thpdocker .
-docker run --name thp --cap-add=NET_ADMIN -d -it -p 22:22 zhindonm/thpdocker -c 'start.sh'
-echo "Container started dettached use:"
-echo "docker attach thp"
+docker run --name thp --cap-add=NET_ADMIN -d -it -p 22:22 zhindonm/thpdocker -c './start.sh'
+echo "Container started in dettached mode - use:"
+echo "\t docker attach thp"
