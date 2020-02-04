3af3afb6c307a9de240854b8cdb4bcf5cbe09e44
thinkpadP1 <zhindonm@gmail.com>
2020-02-04 19:26:19 +0000

    configured thp listeners

--- start.sh
+++ start.sh
@@ -4,10 +4,13 @@
 echo "Initialized start script"
 echo "Loading iptables configuration: ./iptables.rules"
 ./iptables.rules
+echo ""
 echo "Initializing portmap service: service portmap start"
 service portmap start
+echo ""
 echo "Install the fake service translations: pmap_set < fakerpc"
 pmap_set < fakerpc
+echo ""
 echo "Initializing xinetd service: service xinetd start"
 service xinetd start
 echo "Start script DONE"
