3ffc09c795fd6149e2da8eb38503ebfb5a7c2848
thinkpadP1 <zhindonm@gmail.com>
2020-02-04 18:51:12 +0000

    fixed exec start.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ docker build -t zhindonm/thpdocker .
 echo "Docker container BUILT"
 echo ""
 docker run --name thp --cap-add=NET_ADMIN -d -it -p 22:22 zhindonm/thpdocker
+echo ""
 docker exec thp bash start.sh
 echo "Container started in dettached mode"
 echo "use:        docker attach thp"
